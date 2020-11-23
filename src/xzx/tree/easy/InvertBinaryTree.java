3930f797b8906b664fc3509b0576c96f79dd545a
xzx <850449280@qq.com>
2020-11-23 15:45:20 +0800

    今天就这样吧，收工！

---
+++ src/xzx/tree/easy/InvertBinaryTree.java
@@ -0,0 +1,55 @@
+package xzx.tree.easy;
+
+import xzx.sword2offer.problem.structure.TreeNode;
+
+/**
+ * 翻转一棵二叉树。
+ *
+ * 示例：
+ *
+ * 输入：
+ *
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ * 输出：
+ *
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/invert-binary-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author xzx
+ * @date 2020/11/23 14/32
+ */
+public class InvertBinaryTree {
+    /**
+     * 就是个简单递归：
+     * 递归边界：root为null时返回null
+     * 每一次执行左右交换
+     *
+     * @author xzx
+     * @date 2020/11/23 14:49
+     * @param root
+     * @return xzx.sword2offer.problem.structure.TreeNode
+     */
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode left = root.left;
+        root.left = root.right;
+        root.right = left;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
---
+++ src/xzx/tree/easy/IsSameTree.java
@@ -0,0 +1,69 @@
+package xzx.tree.easy;
+
+import xzx.sword2offer.problem.structure.TreeNode;
+
+/**
+ * 给定两个二叉树，编写一个函数来检验它们是否相同。
+ *
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ *
+ * 示例 1:
+ *
+ * 输入:       1         1
+ *           / \       / \
+ *          2   3     2   3
+ *
+ *         [1,2,3],   [1,2,3]
+ *
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入:      1          1
+ *           /           \
+ *          2             2
+ *
+ *         [1,2],     [1,null,2]
+ *
+ * 输出: false
+ * 示例 3:
+ *
+ * 输入:       1         1
+ *           / \       / \
+ *          2   1     1   2
+ *
+ *         [1,2,1],   [1,1,2]
+ *
+ * 输出: false
+ * 通过次数156,486提交次数260,033
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/same-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author xzx
+ * @date 2020/11/23 15/27
+ */
+public class IsSameTree {
+    /**
+     * 同时遍历两个树
+     * @param p
+     * @param q
+     * @return
+     */
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p != null && q != null) {
+            if (p.val == q.val) {
+                return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+            } else {
+                return false;
+            }
+        }
+        if (p == null && q != null || p != null && q == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    
+}
